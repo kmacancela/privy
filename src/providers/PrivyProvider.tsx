7f7906ae374d568182ce6b8370d934dbff4c523b
Karina Macancela <karina@Karinas-MBP.lan>
2025-09-13 16:06:21 -0400

    adding privy sdk

---
+++ src/providers/PrivyProvider.tsx
@@ -0,0 +1,20 @@
+import {PrivyProvider} from '@privy-io/react-auth';
+
+export default function Providers({children}: {children: React.ReactNode}) {
+  return (
+    <PrivyProvider
+      appId="cmfioymvy00tzl50cp6xeg6er"
+      clientId="your-app-client-id"
+      config={{
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          ethereum: {
+            createOnLogin: 'users-without-wallets'
+          }
+        }
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
